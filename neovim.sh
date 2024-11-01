d721c0ae0faee2ba4098b8eb9c95eb4bebe4f477
Nemausa <tappanmorris@outlook.com>
2024-11-01 19:59:34 +0800

    feat: add neovim

---
+++ neovim.sh
@@ -0,0 +1,10 @@
+#bash
+
+sudo apt install curl
+curl -fsSL https://deb.nodesource.com/setup_22.x | sudo -E bash -
+sudo apt install -y nodejs
+node -v
+npm -v
+
+sudo apt install clangd
+git clone git@github.com:Nemausa/neovim-config.git ~/.config/nvim && nvim

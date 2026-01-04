f05ad38610a7cb1357b95f2f04ae208ff09d5ce4
nemausa <45915556+nemausa@users.noreply.github.com>
2026-01-04 18:19:20 +0800

    Fix URL typo in qt-install.sh

--- qt-install.sh
+++ qt-install.sh
@@ -1,1 +1,2 @@
+ulr = https://mirrors.tuna.tsinghua.edu.cn/qt/official_releases/online_installers/
 ./qt-unified-linux-x64-4.6.0-online.run -mirror https://mirrors.tuna.tsinghua.edu.cn/qt/official_releases/online_installers/
